4c6898f9ef83c8f92e80d210e483072710184cf4
Edilson Santos <128603807+EDJR94@users.noreply.github.com>
2023-09-28 20:37:50 -0300

    Update sentiment_analysis_model.py

--- sentiment_analysis_model.py
+++ sentiment_analysis_model.py
@@ -5,6 +5,7 @@ import pickle
 import nltk
 import os
 from nltk.corpus import stopwords
+from nltk.corpus.reader import wordnet
 
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
